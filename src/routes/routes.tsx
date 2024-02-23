68252c08284f9afd072bc28c0b992aa36a60826b
Suyel Haque <slsuyel@gmail.com>
2024-02-23 21:40:55 +0600

    conditional route in route page: comment

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,17 +4,24 @@ import { routeGenerator } from "../utils/routesGenerator";
 import { adminPaths } from "./admin.route";
 import Login from "../pages/Auth/Login";
 
+const admin = true;
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
 
-  {
-    path: "/admin",
-    element: <App />,
-    children: routeGenerator(adminPaths),
-  },
+  admin
+    ? {
+        path: "/admin",
+        element: <App />,
+        children: routeGenerator(adminPaths),
+      }
+    : {
+        path: "/login",
+        element: <Login />,
+      },
 
   {
     path: "/login",
@@ -23,3 +30,47 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
+
+/* import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import { routeGenerator } from "../utils/routesGenerator";
+import { adminPaths } from "./admin.route";
+import Login from "../pages/Auth/Login";
+
+const admin = true;
+
+let routes = [];
+
+if (admin) {
+  routes.push(
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/admin",
+      element: <App />,
+      children: routeGenerator(adminPaths),
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    }
+  );
+} else {
+  routes.push(
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    }
+  );
+}
+
+const router = createBrowserRouter(routes);
+
+export default router;
+ */
